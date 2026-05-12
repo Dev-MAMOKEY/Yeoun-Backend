6b94d15df75aba41df854fe6406f8bf115f51cae
kdh <soccerkdh12@naver.com>
2026-05-12 14:31:06 +0900

    Initial commit

---
+++ src/main/java/com/mamokey/yeoun/YeounApplication.java
@@ -0,0 +1,13 @@
+package com.mamokey.yeoun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeounApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeounApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamokey/yeoun/YeounApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamokey.yeoun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeounApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
