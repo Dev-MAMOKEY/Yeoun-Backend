6de973431505953bddace1048942e2d5369ddd55
kdh <soccerkdh12@naver.com>
2026-05-13 10:29:38 +0900

    [feat/#3] feat: GlobalEntity 파일 구현

---
+++ src/main/java/com/mamokey/yeoun/global/entity/GlobalEntity.java
@@ -0,0 +1,38 @@
+package com.mamokey.yeoun.global.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(GlobalEntityListener.class)
+public class GlobalEntity {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public void softDelete() { // 멤버 제명(소프트 딜리트) 위한 메서드
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public boolean isDealeted() {
+        return this.deletedAt != null;
+    }
+}
